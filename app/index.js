4873a105d9539c6ac206c3ca23ac9c8c5d5529aa
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-24 22:13:17 +0530

    Added animation of fadein and fixed container styles

--- app/index.js
+++ app/index.js
@@ -3,18 +3,19 @@ import { StatusBar } from 'expo-status-bar'
 import { Image, StyleSheet, View } from 'react-native'
 import { wp, hp } from '../helpers/common'
 import { LinearGradient } from 'expo-linear-gradient'
+import Animated, { FadeInDown } from 'react-native-reanimated'
 export default function WelcomeScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <StatusBar style="light" />
             <Image source={require('../assets/images/welcome.png')} style={styles.bgImage} resizeMode="cover" />
-            <View style={styles.container}>
+            <Animated.View entering={FadeInDown.duration(800)} style={styles.container}>
                 <LinearGradient
                     colors={['rgba(255,255,255,0)', 'rgba(255,255,255,0.5)', 'white', 'white']}
                     style={styles.gradient}
                     start={{ x: 0.5, y: 0 }}
                     end={{ x: 0.5, y: 0.8 }} />
-            </View>
+            </Animated.View>
         </View>
     )
 }
@@ -31,7 +32,7 @@ const styles = StyleSheet.create({
     gradient: {
         width: wp(100),
         height: hp(65),
-        position: 'absolute',
         bottom: 0,
+        position: 'absolute',
     }
 })
