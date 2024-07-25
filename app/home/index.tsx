57413789c40c7b62cebff8fdc144db64bf1d7361
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-25 19:27:57 +0530

    Added home screen layout and styles

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,14 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useRef, useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { Feather, FontAwesome6, Ionicons } from '@expo/vector-icons';
+import { theme } from '@/constants/theme';
+import { hp, wp } from '@/helpers/common';
 
 const HomeScreen = () => {
+    const { top } = useSafeAreaInsets();
+    const paddingTop = top > 0 ? top + 10 : 30;
+    const [searchQuery, setSearchQuery] = useState('');
+    const searchInputRef = useRef(null);
     return (
-        <View>
-            <Text>HomeScreen</Text>
+        <View style={[styles.container, { paddingTop }]}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Pressable>
+                    <Text style={styles.title}>Pexels</Text>
+                </Pressable>
+                <Pressable>
+                    <FontAwesome6 name="bars-staggered" size={22} color={theme.colors.neutral(0.7)} style={styles.title} />
+                </Pressable>
+            </View>
+
+            {/* Search Bar */}
+            <ScrollView contentContainerStyle={{ gap: 15 }}>
+                <View style={styles.searchBar}>
+                    <View style={styles.searchIcon}>
+                        <Feather name="search" size={24} color={theme.colors.neutral(0.4)} />
+                    </View>
+                    <TextInput placeholder='Search for Wallpapers...' style={styles.searchInput} value={searchQuery} onChangeText={value => setSearchQuery(value)} ref={searchInputRef} />
+                    {searchQuery && (<Pressable style={styles.closeIcon}>
+                        <Ionicons name="close" size={24} color={theme.colors.neutral(0.6)} />
+                    </Pressable>)}
+                </View>
+            </ScrollView>
         </View>
     )
 }
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        gap: 15
+    },
+    header: {
+        marginHorizontal: wp(4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: hp(4),
+        fontWeight: theme.fontWeights.semibold as any,
+        color: theme.colors.neutral(0.9)
+    },
+    searchBar: {
+        marginHorizontal: wp(4),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: theme.colors.grayBG,
+        backgroundColor: theme.colors.white,
+        padding: 6,
+        paddingLeft: 10,
+        borderRadius: theme.radius.lg,
+    },
+
+
+})
