ed64eb07a56a360747817f92f9791bc8d71c4f42
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-08 12:59:25 +0530

    Added two api methods and its helper function

---
+++ helpers/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const apiUrl = API_URL + `?key=${API_KEY}`;
+const formatUrl = (params: any) => {
+    let url = apiUrl + "&per_page=25&safeSearch=true&editors_choices=true";
+    if (!params) return url;
+    let paramKeys = Object.keys(params);
+    paramKeys.map((key: any) => {
+        let value = key == 'q' ? encodeURIComponent(params[key]) : paramKeys[key];
+        url += `&${key}=${value}`;
+    });
+    console.log(url);
+    return url;
+}
+
+
+export const fetchData = async (params: any) => {
+    try {
+        const res = await axios.get(formatUrl(params));
+        return { success: true, data: res.data.hits };
+    } catch (error: any) {
+        console.log(error.message);
+        return { success: false, message: error }
+    }
+}
