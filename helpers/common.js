3c38d399f745fc13b9e9f8b1ad699eee0df7b30c
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-08 12:59:08 +0530

    Added two helper functions

--- helpers/common.js
+++ helpers/common.js
@@ -11,3 +11,25 @@ export const hp = percentage => {
     const height = deviceHeight;
     return (percentage * height) / 100;
 }
+
+export const getColumnCount = () => {
+    if (deviceWidth >= 1024) {
+        return 4;
+    } else if (deviceHeight >= 768) {
+        return 3;
+    } else if (deviceWidth >= 350) {
+        return 2;
+    } else {
+        return 1;
+    }
+}
+
+export const getImageOrientation = (height, width) => {
+    if (width > height) {
+        return 250; //landscape
+    } else if (width < height) {
+        return 300; // portrait
+    } else {
+        return 200; //square
+    }
+}
