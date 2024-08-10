24f45792fc2c1be94f3c63279a5348340cc827bb
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-10 16:57:07 +0530

    added filter constants

--- constants/data.ts
+++ constants/data.ts
@@ -1,5 +1,12 @@
 const categories = ["backgrounds", "fashion", "nature", "science", "education", "feelings", "health", "people", "religion", "places", "animals", "industry", "computer", "food", "sports", "transportation", "travel", "buildings", "business", "music"]
 
+const filters = {
+    order: ["popular", "latest"],
+    orientation: ["vertical", "vertical"],
+    type: ["photo", "illustration", "vector"],
+    colors: ["red", "green", "yellow", "orange", "turquoise", "blue", "white", "black", "brown", "pink", "gray", "purple"],
+}
+
 export const constants = {
-    categories
+    categories, filters
 }
