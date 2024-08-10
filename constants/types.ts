2ce679fdc5c90b0b6abecb6a3429bc1e95db7ce1
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-10 20:23:59 +0530

    Added more types

--- constants/types.ts
+++ constants/types.ts
@@ -21,3 +21,26 @@ export type imageCardPropsType = {
 export type imageMasonryPropsType = {
     data: Object | any;
 }
+
+export type filterSectionPropsType = React.JSX.IntrinsicAttributes & { data: any; filterName: any; filters: any; setFilters: any; }
+
+export type filtersType = {
+    order: string[],
+    orientation: string[],
+    type: string[],
+    colors: string[],
+}
+
+export type sectionsType = {
+    order: (props: filterSectionPropsType) => React.JSX.Element,
+    orientation: (props: filterSectionPropsType) => React.JSX.Element,
+    type: (props: filterSectionPropsType) => React.JSX.Element,
+    colors: (props: filterSectionPropsType) => React.JSX.Element,
+}
+
+export type FilterRowPropsType = {
+    data: any,
+    filterName: string | any,
+    filters: any,
+    setFilters: React.Dispatch<React.SetStateAction<string>> | Function,
+}
